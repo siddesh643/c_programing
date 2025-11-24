c068d9160f3cb9affbad99781f4550efae584586
siddesh643 <siddesh2606@eng.rizvi.edu.in>
2025-11-24 13:52:13 +0530

    Add program to find prime numbers in a range

---
+++ Experiment_4.c
@@ -0,0 +1,40 @@
+/* NAME : siddesh Sudhakaran 
+   UNI  : 251C044
+   Branch: CIVIL
+   DIV : B */
+
+#include <stdio.h>
+
+int main() {
+    int start, end, i, j, isPrime;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+
+    for (i = start; i <= end; i++) {
+
+        if (i < 2)
+            continue;
+
+        isPrime = 1;
+
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+            printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
